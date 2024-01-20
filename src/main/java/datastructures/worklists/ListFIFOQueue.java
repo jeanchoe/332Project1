1417467c4b3fdd91df90bfdcdb45f18751f57a84
Jean Choe <jeannchoee@gmail.com>
2024-01-19 21:44:26 -0800

    Checkpoint 1

--- src/main/java/datastructures/worklists/ListFIFOQueue.java
+++ src/main/java/datastructures/worklists/ListFIFOQueue.java
@@ -1,6 +1,5 @@
 package datastructures.worklists;
 
-//import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.FIFOWorkList;
 
 import java.util.NoSuchElementException;
@@ -10,78 +9,62 @@ import java.util.NoSuchElementException;
  * for method specifications.
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
+
     private Node<E> front;
     private Node<E> back;
-    private int length;
-
-    private static class Node<E>{
-        E data;
-        Node<E> next;
-
-        public Node(E data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public ListFIFOQueue() {
-        //throw new NotYetImplementedException();
-        front = null;
-        back = null;
-        length = 0;
-    }
+    public int size;
+    public ListFIFOQueue() { this.size = 0; }
 
     @Override
     public void add(E work) {
 
-        // throw new NotYetImplementedException();
-        Node<E> newestNode = new Node<>(work);
-
-        // Checking for an empty que,
-        // sets both front and back to the newest node.
-        if(back == null){
-            front = newestNode;
-            back = newestNode;
-        }else {
-            // Enters if there was something in the queue,
-            // then adds the newest node to the end
-            back.next = newestNode;
-            back = newestNode;
+        if (front == null) {
+            front = new Node<>(work);
+            back = front;
+        } else {
+            back.next = new Node<>(work);
+            back = back.next;
         }
-        length++;
+        size++;
     }
-
     @Override
     public E peek() {
-        // throw new NotYetImplementedException();
-        if(front == null){
-            throw new NoSuchElementException("Queue is empty");
-        }
+        if(!hasWork()) throw new NoSuchElementException();
         return front.data;
     }
 
     @Override
     public E next() {
-        //throw new NotYetImplementedException();
-        if (front == null){
-            throw new NoSuchElementException("Queue is empty");
-        }
-        length--;
-        E data = front.data;
+        if(!hasWork()) throw new NoSuchElementException();
+        Node<E> thing = new Node<>(front.data);
         front = front.next;
-        return data;
+        size--;
+        return thing.data;
     }
 
     @Override
     public int size() {
-        // throw new NotYetImplementedException();
-        return length;
+        return size;
     }
 
     @Override
     public void clear() {
-        // throw new NotYetImplementedException();
         front = null;
-        back = null;
-        length = 0;
+        size = 0;
+    }
+
+    public static class Node<E> {
+        public final E data;
+        public Node<E> next;
+
+        public Node(E data) {
+            this(data, null);
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
+
